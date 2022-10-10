26909690dd5a49d99d3379cc7dd612b013a566f9
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-10 21:54:28 +0200

    Create test using by MockMvc test endpoint simulation client

---
+++ src/test/java/pl/szybiak/controller/VideoControllerTest.java
@@ -0,0 +1,22 @@
+package pl.szybiak.controller;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.szybiak.VideoApp1011ApplicationTests;
+
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
+class VideoControllerTest extends VideoApp1011ApplicationTests {
+
+    @Test
+    @DisplayName()
+    
+
+}
