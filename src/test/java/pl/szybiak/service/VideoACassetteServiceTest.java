8fdea29b544d362e0094bbda3001877bc750defa
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-10 15:10:06 +0200

    Refactoring dependency path junit.jupiter, Mockito

--- src/test/java/pl/szybiak/service/VideoACassetteServiceTest.java
+++ src/test/java/pl/szybiak/service/VideoACassetteServiceTest.java
@@ -1,13 +1,28 @@
 package pl.szybiak.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.szybiak.repository.VideoAssettsRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 class VideoACassetteServiceTest {
+
+    @InjectMocks
+    private VideoAssettsRepository videoAssettsRepository;
+
+    @Mock
+    private VideoACassetteService videoACassetteService;
+
+
+
 
     @Test
     void findById() {
+        System.out.println();
     }
 
     @Test
