380ffbb17199088ae74aa5f57024e7ec8826f215
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-10 22:13:07 +0200

    add jsonPath test isNot Empty, and is exists

--- src/test/java/pl/szybiak/controller/VideoControllerTest.java
+++ src/test/java/pl/szybiak/controller/VideoControllerTest.java
@@ -8,6 +8,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import pl.szybiak.VideoApp1011ApplicationTests;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 @SpringBootTest
@@ -16,7 +19,20 @@ import pl.szybiak.VideoApp1011ApplicationTests;
 class VideoControllerTest extends VideoApp1011ApplicationTests {
 
     @Test
-    @DisplayName()
-    
+    @DisplayName("Should return list of Videocasette, at the least entry")
+    void shouldReturnListOffVidoecassete() throws Exception {
+        var url = "/cassetts/all";
+
+        mockMvc.perform(get(url))
+                .andDo(print())
+                //then
+                .andExpect(jsonPath("$[*].id").isNotEmpty())
+                .andExpect(jsonPath("$[0].id").exists())
+                .andExpect(jsonPath("$[0].title").exists())
+                .andExpect(jsonPath("$[0].productionYear").exists());
+
+
+    }
+
 
 }
